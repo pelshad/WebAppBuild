6874649788020c94da98f1a8b125e6a1e2ce0f4e
LuluPresso <105760017+pelshad@users.noreply.github.com>
2022-11-03 21:50:09 +0900

    Update App.js

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { WebView } from "react-native-webview";
 
 export default function App() {
   return Platform.OS === "web" ? (
-    <iframe src="http://localhost:8080/" height={'100%'} width={'100%'} style={{ border: "none" }} />
+    // 넣을 홈페이지 주소
+    <iframe src="http://localhost:8080/" height={'100%'} width={'100%'} style={{ border: "none" }} /> 
   ) : (
     <View style={{ flex: 1 }}>
       <WebView
-        source={{ uri: "http://localhost:8080/" }}
+        source={{ uri: "http://localhost:8080/" }} // 넣을 홈페이지 주소
       />
     </View>
   )
